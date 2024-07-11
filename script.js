315b9a7fe33416d28811fa895bbcff9e48369a73
suresh-skj <sureshjanwa20@gmail.com>
2024-07-11 23:33:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 <h1>Why I want to become a developer?</h1>
-	<p>i want to become  a software devloper
+	<p>i want to become  a software devloper</P>
 <script type="text/javascript" src="./script.js">
